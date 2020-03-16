15e9d8e2ab99897c17371906c1566f11d800ed63
limwin94 <amuguna94@gmail.com>
2020-03-16 15:25:19 +0900

    collectionView cell의 커스텀 클래스 생성 및 지정

---
+++ PhotosApp/PhotosApp/View/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  PhotosApp
+//
+//  Created by 임승혁 on 2020/03/16.
+//  Copyright © 2020 임승혁. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
