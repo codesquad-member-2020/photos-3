eead33cf8aec98318b54a1f1cd95c592e3dd6abc
limwin94 <amuguna94@gmail.com>
2020-03-16 14:30:34 +0900

    DataSource 프로토콜 채택

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
+        
+        cell.backgroundColor = .black
+        
+        return cell
+    }
+    
+    
+}
