d004e69496e0d4685ba036433c409f982b8068b9
seizze <cwcwc3@gmail.com>
2020-03-16 14:36:06 +0900

    컬렉션뷰 랜덤 색상 설정

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -26,7 +26,10 @@ extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
         
-        cell.backgroundColor = .black
+        cell.backgroundColor = UIColor(red: CGFloat.random(in: 0...1),
+                                       green: CGFloat.random(in: 0...1),
+                                       blue: CGFloat.random(in: 0...1),
+                                       alpha: 1)
         
         return cell
     }
