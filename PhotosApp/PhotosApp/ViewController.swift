3437cb4102f4daf7b55808028548b103063a5682
limwin94 <amuguna94@gmail.com>
2020-03-16 14:17:14 +0900

    프로젝트 생성

---
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotosApp
+//
+//  Created by 임승혁 on 2020/03/16.
+//  Copyright © 2020 임승혁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
