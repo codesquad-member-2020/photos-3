95e738019531a4baf3da0e668b520d195598163e
limwin94 <amuguna94@gmail.com>
2020-03-17 17:08:41 +0900

    PHCachingImageManager 클래스를 활용해서 Cell에 표시하기.

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -10,25 +10,46 @@ import UIKit
 import Photos
 
 class ViewController: UIViewController {
-
+    let imageManager = PHCachingImageManager()
+    var allPhotos: PHFetchResult<PHAsset>!
+    var thumbnailSize: CGSize!
+    
+    @IBOutlet weak var collectionViewFlowLayout: UICollectionViewFlowLayout!
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = self
         title = "Photos"
+        
+        allPhotos = PHAsset.fetchAssets(with: nil)
+        setThumbnailSize()
+    }
+    
+    func setThumbnailSize() {
+        let scale = UIScreen.main.scale
+        let cellSize = collectionViewFlowLayout.itemSize
+        thumbnailSize = CGSize(width: cellSize.width * scale, height: cellSize.height * scale)
     }
 }
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 40
+        return allPhotos.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
+        print(indexPath.item)
+        let asset = self.allPhotos.object(at: indexPath.item)
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as? PhotoCell else { fatalError("collection view error") }
         
-        cell.backgroundColor = UIColor.randomColor()
+        cell.representedAssetIdentifier = asset.localIdentifier
+        
+        imageManager.requestImage(for: asset, targetSize: thumbnailSize, contentMode: .aspectFill, options: nil) { image, _ in
+            if cell.representedAssetIdentifier == asset.localIdentifier {
+                cell.setPhoto(image!)
+            }
+        }
         
         return cell
     }
