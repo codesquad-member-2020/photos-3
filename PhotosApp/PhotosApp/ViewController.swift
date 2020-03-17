7524bfe8b5d0e50193f09a6a7847fe1e628e6e10
seizze <cwcwc3@gmail.com>
2020-03-17 16:40:18 +0900

    feat: 사진 라이브러리 변화시에 컬렉션뷰를 다시 로드

--- PhotosApp/PhotosApp/ViewController.swift
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -10,13 +10,29 @@ import UIKit
 import Photos
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    var fetchResult: PHFetchResult<PHAsset>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = self
         title = "Photos"
+        
+        PHPhotoLibrary.shared().register(self)
+    }
+    
+    deinit {
+        PHPhotoLibrary.shared().unregisterChangeObserver(self)
+    }
+}
+
+extension ViewController: PHPhotoLibraryChangeObserver {
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        guard let changes = changeInstance.changeDetails(for: fetchResult) else { return }
+        fetchResult = changes.fetchResultAfterChanges
+        DispatchQueue.main.sync { collectionView.reloadData() }
     }
 }
 
@@ -27,9 +43,7 @@ extension ViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
-        
         cell.backgroundColor = UIColor.randomColor()
-        
         return cell
     }
 }
