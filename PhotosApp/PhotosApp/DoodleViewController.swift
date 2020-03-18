3d7b8d6f99a63a486879ac5e2e42e6019c09687b
seizze <cwcwc3@gmail.com>
2020-03-18 20:51:30 +0900

    feat: Doodle VC 추가

---
+++ PhotosApp/PhotosApp/DoodleViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DoodleViewController.swift
+//  PhotosApp
+//
+//  Created by Chaewan Park on 2020/03/18.
+//  Copyright © 2020 임승혁. All rights reserved.
+//
+
+import UIKit
+
+class DoodleViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Doodles"
+        collectionView.backgroundColor = .darkGray
+    }
+}
